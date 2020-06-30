69c6c63d483ae129b601e1b7317057d8947352c8
Anton <kam_ant1983@mail.ru>
2020-06-30 20:52:50 +0300

    footer

--- #source/js/script.js
+++ #source/js/script.js
@@ -349,3 +349,23 @@ $(window).scroll(function () {
 		$('.header').removeClass('active');
 	}
 });
+
+
+for (let i of document.querySelectorAll(".about__number")) {
+
+	let numberTop = i.getBoundingClientRect().top,
+		start = +i.innerHTML,
+		end = +i.dataset.max;
+
+	window.addEventListener('scroll', function onScroll() {
+		if (window.pageYOffset > numberTop - window.innerHeight / 2) {
+			this.removeEventListener('scroll', onScroll);
+			let interval = this.setInterval(function () {
+				i.innerHTML = ++start;
+				if (start == end) {
+					clearInterval(interval);
+				}
+			}, 10);
+		}
+	});
+}
